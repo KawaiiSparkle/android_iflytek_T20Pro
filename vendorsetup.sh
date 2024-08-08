86feb389959e9adf78a380f746dc38479fba70bc
亻尔 女子 <156587835+KawaiiSparkle@users.noreply.github.com>
2024-08-08 16:22:07 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_ctg3-user
-add_lunch_combo omni_ctg3-userdebug
-add_lunch_combo omni_ctg3-eng
+add_lunch_combo ctg3-user
+add_lunch_combo ctg3-userdebug
+add_lunch_combo ctg3-eng
